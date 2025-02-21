0523d5dd78ec43375094cca5af3ec3b723a9adb6
jfrey <nomhad@gmail.com>
2025-02-21 17:45:11 +0100

    go with AGPL3

--- src/ExtendedPlugin.hpp
+++ src/ExtendedPlugin.hpp
@@ -31,7 +31,7 @@ protected:
   fix16_t buffIn[BUFFER_SIZE];
   fix16_t buffOut[BUFFER_SIZE];
 
-  const char *getLicense() const override { return "http://ullo.fr/botania/custom_licence"; }
+  const char *getLicense() const override { return "AGPL3"; }
 
   // The following functions will be called upon encountering MIDI events, to be implemented by subclasses. channel: 0..15. frame: frame number within the buffer of the MIDI event
   virtual void noteOn(uint8_t /*note*/, uint8_t /*velocity*/, uint8_t /*channel*/, uint32_t /*frame*/) {};
